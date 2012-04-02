432a44073ac3dda828835d6eba827cdc3bf17918
Fernando García Samblas <fernando.garcia@the-cocktail.com>
2012-04-02 22:12:33 +0200

    probando

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,4 @@
-WICKED_PDF = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
+WickedPdf.config = {
   :exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+  #:exe_path => Rails.root.join('bin', 'wkhtmltopdf-i386').to_s,
 }
