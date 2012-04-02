77d3b44625cc6f81eb9850388ea296c19b4ed770
Fernando García Samblas <fernando.garcia@the-cocktail.com>
2012-04-02 22:02:42 +0200

    probando

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -3,6 +3,3 @@ WICKED_PDF = {
   #:layout => "pdf.html",
   :exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
 }
-PDFKit.configure do |config|
-  config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
-end
