3a8519b86fb3acae8cd6cb5838c9ca183a796ca9
Fernando García Samblas <fernando.garcia@the-cocktail.com>
2012-04-03 19:30:17 +0200

    Retoques

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,12 @@ Openflydb::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  config.after_initialize do
+    WickedPdf.config = {
+      :exe_path => "/usr/local/bin/wkhtmltopdf"
+    }
+  end
+
 end
 
