76546de7764fb1ba1500863150c52c0887530c86
nando <fernando.garcia@the-cocktail.com>
2012-04-19 21:58:18 +0200

    ffffuck!

--- db/migrate/20120419105600_add_fsdb_id.rb
+++ db/migrate/20120419105600_add_fsdb_id.rb
@@ -1,6 +1,5 @@
 class AddFsdbId < ActiveRecord::Migration
   def self.up
-    remove_column :pilots, :fsdb_id
     add_column :pilots, :fsdb_id, :integer
     add_index :pilots, :fsdb_id, :unique => true
     Pilot.all.each {|p| p.update_attribute :fsdb_id, p.id}
