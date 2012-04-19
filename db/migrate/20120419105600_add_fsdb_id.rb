b002bcfd1e636d6a8223aae45558cbe9f87afe57
nando <fernando.garcia@the-cocktail.com>
2012-04-19 21:47:03 +0200

    fsdb_id

---
+++ db/migrate/20120419105600_add_fsdb_id.rb
@@ -0,0 +1,11 @@
+class AddCivlid < ActiveRecord::Migration
+  def self.up
+    add_column :pilots, :fsdb_id, :string
+    add_index :fsdb_id, :unique => true
+    Pilot.all.each {|p| p.update_attribute :fsdb_id, p.id}
+  end
+
+  def self.down
+    remove_column :pilots, :fsdb_id
+  end
+end
