50143c07d5fc7d07f10ebf0a20fd85053c358992
Fernando García Samblas <fernando.garcia@the-cocktail.com>
2012-04-26 00:36:11 +0200

    retoques en las vistas

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,4 +1,4 @@
+puts Rails.root.join('bin', "wkhtmltopdf-#{Rails.env == :production ? 'amd64' : 'i386'}").to_s
 WickedPdf.config = {
-  :exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
-  #:exe_path => Rails.root.join('bin', 'wkhtmltopdf-i386').to_s,
+  :exe_path => Rails.root.join('bin', "wkhtmltopdf-#{Rails.env == :production ? 'amd64' : 'i386'}").to_s
 }
