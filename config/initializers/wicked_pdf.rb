b198a7e6b5359b61dcf2c45b714499bbadbb37cf
Fernando Garcia Samblas <fernando.gs@gmail.com>
2013-03-31 20:36:11 +0200

    /app

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -2,6 +2,6 @@
 WickedPdf.config = {
     #:exe_path => Rails.root.join('bin', "wkhtmltopdf-amd64").to_s
     #:exe_path => Rails.root.join('bin', "wkhtmltopdf-i386").to_s
-  :exe_path => 'vendor/bundle/ruby/1.9.1/bundler/gems/wkhtmltopdf-heroku-ad9f8e5f83fe/bin/wkhtmltopdf-linux-amd64'
+  :exe_path => '/app/vendor/bundle/ruby/1.9.1/bundler/gems/wkhtmltopdf-heroku-ad9f8e5f83fe/bin/wkhtmltopdf-linux-amd64'
 }
 #end
