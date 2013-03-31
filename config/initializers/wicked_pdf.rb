e07d0f712f7c7ee6f5e3ae9b10e737de5b7445d1
Fernando Garcia Samblas <fernando.gs@gmail.com>
2013-03-31 21:28:28 +0200

    lo dejamos como teóricamente debería funcionar

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,7 +1,6 @@
-#if Rails.env.development?
-WickedPdf.config = {
+if Rails.env.development?
+  WickedPdf.config = {
     #:exe_path => Rails.root.join('bin', "wkhtmltopdf-amd64").to_s
-    #:exe_path => Rails.root.join('bin', "wkhtmltopdf-i386").to_s
-  :exe_path => '/app/vendor/bundle/ruby/1.9.1/bundler/gems/wkhtmltopdf-heroku-ad9f8e5f83fe/bin/wkhtmltopdf-linux-amd64'
-}
-#end
+    :exe_path => Rails.root.join('bin', "wkhtmltopdf-i386").to_s
+  }
+end
