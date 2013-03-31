e8b08dd674b46d6c93d9a9ff81429a05584d86e5
Fernando Garcia Samblas <fernando.gs@gmail.com>
2013-03-31 20:28:54 +0200

    palos de ciego en heroku

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,6 +1,7 @@
 #if Rails.env.development?
-#  WickedPdf.config = {
+WickedPdf.config = {
     #:exe_path => Rails.root.join('bin', "wkhtmltopdf-amd64").to_s
     #:exe_path => Rails.root.join('bin', "wkhtmltopdf-i386").to_s
-#  }
+  :exe_path => 'vendor/bundle/ruby/1.9.1/bundler/gems/wkhtmltopdf-heroku-ad9f8e5f83fe/bin/wkhtmltopdf-linux-amd64'
+}
 #end
